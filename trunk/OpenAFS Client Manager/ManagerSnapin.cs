627c417e0985d721516e55e6488956e0913d7600
brantgurga <brantgurga@269046b0-6396-11de-9f54-6d4fae1110e3>
2009-09-04 04:21:21 +0000

    ignore visual studio outputs create tree in snap-in

--- trunk/OpenAFS Client Manager/ManagerSnapin.cs
+++ trunk/OpenAFS Client Manager/ManagerSnapin.cs
@@ -21,13 +21,41 @@ namespace OpenAFSClientManager
             this.RootNode = new ScopeNode();
             this.RootNode.DisplayName = "OpenAFS Client Manager";
 
+            ScopeNode cifsNode = new ScopeNode(true);
+            ScopeNode perfNode = new ScopeNode(true);
+            ScopeNode cacheManNode = new ScopeNode(true);
+            ScopeNode freelanceNode = new ScopeNode(true);
+            ScopeNode serversNode = new ScopeNode(true);
+            ScopeNode logonNode = new ScopeNode(true);
+            ScopeNode diagnosticsNode = new ScopeNode(true);
+            cifsNode.DisplayName = "CIFS";
+            perfNode.DisplayName = "Performance";
+            cacheManNode.DisplayName = "Cache Manager";
+            freelanceNode.DisplayName = "Freelance Client";
+            serversNode.DisplayName = "Servers";
+            logonNode.DisplayName = "Log on options";
+            diagnosticsNode.DisplayName = "Diagnostics";
+            this.RootNode.Children.Add(cifsNode);
+            this.RootNode.Children.Add(perfNode);
+            this.RootNode.Children.Add(cacheManNode);
+            this.RootNode.Children.Add(freelanceNode);
+            this.RootNode.Children.Add(serversNode);
+            this.RootNode.Children.Add(logonNode);
+            this.RootNode.Children.Add(diagnosticsNode);
+
             FormViewDescription fvd = new FormViewDescription();
             fvd.DisplayName = "OpenAFS Client Manager";
             fvd.ViewType = typeof(ManagerFormView);
             fvd.ControlType = typeof(ManagerControl);
 
+            cifsNode.ViewDescriptions.Add(fvd);
+            perfNode.ViewDescriptions.Add(fvd);
+            cacheManNode.ViewDescriptions.Add(fvd);
+            freelanceNode.ViewDescriptions.Add(fvd);
+            serversNode.ViewDescriptions.Add(fvd);
+            logonNode.ViewDescriptions.Add(fvd);
+            diagnosticsNode.ViewDescriptions.Add(fvd);
             this.RootNode.ViewDescriptions.Add(fvd);
-            this.RootNode.ViewDescriptions.DefaultIndex = 0;
         }
     }
 }
